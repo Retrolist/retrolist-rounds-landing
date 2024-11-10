50f87bf5bc2583defcd8a3ed36174427d5366969
“Chinapanda” <kittikornchina2014@gmail.com>
2024-11-10 08:07:43 +0700

    feat: update date

--- src/App.tsx
+++ src/App.tsx
@@ -145,8 +145,8 @@ function App() {
             <RetrolistRound
               link="https://round6.retrolist.app"
               title="Round 6: Governance"
-              date="Oct 1 - Nov 7, 2024"
-              votingPeriod="Oct 28 - Nov 7, 2024"
+              date="Oct 1 - Nov 13, 2024"
+              votingPeriod="Oct 31 - Nov 13, 2024"
               medianFundingAwarded="21,266"
               projectRewarded="85"
               topCategories="DeFi"
