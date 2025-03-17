39085a4bbeb2fec7e7983c57162732e7b40d8287
“Chinapanda” <kittikornchina2014@gmail.com>
2025-03-17 16:59:25 +0700

    update user feedback

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
+import { UserbackProvider } from "@userback/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserbackProvider token="A-qtLvQIC1pFYrlXuqe0EdDUF9e">
+      <App />
+    </UserbackProvider>
+  </StrictMode>
+);
